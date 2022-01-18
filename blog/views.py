ed37487c03001b3b70dab29d0418119108d8055c
Nahyun Kim <knh4769@khu.ac.kr>
2022-01-18 16:13:40 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-from django.views.generic import ListView       
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView 
 from .models import Post
 
 # CBV방식으로 구현
-# FBV 스타일의 index() 함수를 대체하는 PostList 클래스를 ListView 클래스를 상속하여 만듦
-class PostList(ListView):
+class PostList(ListView):           # FBV 스타일의 index() 함수를 대체하는 PostList 클래스를 ListView 클래스를 상속하여 만듦
     model = Post
     ordering = '-pk'
+
+class PostDetail(DetailView):       # FBV 스타일의 single_post_page 함수를 대체하는 PostDetail 클래스
+    model = Post
+
 
 # FBV방식으로 구현
 # def index(request):
@@ -21,13 +24,13 @@ class PostList(ListView):
 #         }
 #     )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)      # Post.objects.get(): 괄호 안의 조건을 만족하는 Post 레코드를 가져오라
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)      # Post.objects.get(): 괄호 안의 조건을 만족하는 Post 레코드를 가져오라
 
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post,
+#         }
+#     )
